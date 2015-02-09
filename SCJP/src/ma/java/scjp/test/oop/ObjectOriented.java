a1a7972346836c5e80265b2cc611918169d7576f
adil errady <errady.adil@gmail.com>
2015-02-09 14:40:40 +0100

    Organizing SCJP packages as in the book chapters

---
+++ SCJP/src/ma/java/scjp/test/oop/ObjectOriented.java
@@ -0,0 +1,48 @@
+package ma.java.scjp.test.oop;
+
+
+public class ObjectOriented
+{
+
+	public static void main(final String[] args)
+	{
+		final Vehicle v = new Car();
+		//		final Bike b = (Bike) v;
+
+		//		v.printSound();
+		//		b.printSound();
+
+	}
+}
+
+
+class Vehicle
+{
+	public void printSound()
+	{
+		System.out.print("vehicle");
+	}
+}
+
+class Car extends Vehicle
+{
+	@Override
+	public void printSound()
+	{
+		System.out.print("car");
+	}
+}
+
+class Bike extends Vehicle
+{
+	@Override
+	public void printSound()
+	{
+		System.out.print("bike");
+	}
+}
+
+class Decathlon extends Bike
+{
+
+}
