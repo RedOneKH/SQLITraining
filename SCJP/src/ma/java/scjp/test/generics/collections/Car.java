85e9eb9920a72767bad434cd8b850b870e4d0ef9
adil errady <errady.adil@gmail.com>
2015-02-23 09:45:42 +0000

    Updating Generics/collections

---
+++ SCJP/src/ma/java/scjp/test/generics/collections/Car.java
@@ -0,0 +1,18 @@
+package ma.java.scjp.test.generics.collections;
+
+public class Car
+{
+
+    private String name;
+    private int passengerCapacity;
+    
+    public Car(String name, int passengerCapacity) {
+        this.name = name;
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return name+" / Capacity: "+passengerCapacity;
+    }
+}
