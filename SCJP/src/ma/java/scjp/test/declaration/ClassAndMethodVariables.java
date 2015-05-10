f8a2cb465a972f368fee24ef04c99c1fe08e1c57
Adil Rady <adil.ensias@gmail.com>
2015-05-10 18:48:20 +0000

    Adding new files to some packages

---
+++ SCJP/src/ma/java/scjp/test/declaration/ClassAndMethodVariables.java
@@ -0,0 +1,12 @@
+package ma.java.scjp.test.declaration;
+
+public class ClassAndMethodVariables {
+
+	int classVariable;
+	
+	public void method(){
+		int methodVariable;
+		System.out.println(classVariable); //Get default value
+//		System.out.println(methodVariable);//Does not get defautl value : Compiler Error
+	}
+}
--- SCJP/src/ma/java/scjp/test/flowcontrol/exception/assertion/SwitchTest.java
+++ SCJP/src/ma/java/scjp/test/flowcontrol/exception/assertion/SwitchTest.java
@@ -7,12 +7,12 @@ public class SwitchTest {
 		
 		switch(color){	// color should be constant and can be :int, short, byte, char or enum 
 		
+			default : System.out.println("Sorry, it's not a color!");
+			case "Yellow"	: System.out.println("It's yellow!"); break;
 			case "Green"	: System.out.println("It's green!"); break;
 			case "Pink"		: System.out.println("It's pink!"); break;
-			case "Yellow"	: System.out.println("It's yellow!");
 			//If default is somewhere above Yellow case it will not be executed ! even that Yellow case
 			//has not a break statement
-			default : System.out.println("Sorry, it's not a color!"); 	
 			
 		}
 	}
